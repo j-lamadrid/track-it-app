98a68c01c55304ffd074b417366d0c8031ccb6e2
Jacob Lamadrid <jlamadridix@gmail.com>
2024-05-25 00:27:07 -0700

    home page + other pages template

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TrackiT',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'TrackiT Home Page'),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.white,
+            brightness: Brightness.light,
+          ),
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(
+              fontSize: 16.0,
+              color: Colors.white,
+            ),
+            titleLarge: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          scaffoldBackgroundColor: Colors.white, // Light gray background
+          primaryIconTheme: const IconThemeData(color: Colors.white60), // White icons
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white60,
+            foregroundColor: Colors.white,
+          ),
+        ),
+        home: const MyHomePage(title: 'TrackiT Home Page')
     );
   }
 }
@@ -55,18 +60,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,49 +70,253 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Container(
+          height: 600,
+          width: 400,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+              colors: [Colors.blueAccent[400]!, Colors.lightBlue[100]!, Colors.yellow[400]!], // Adjust color shades as desired
+              begin: Alignment.topLeft, // Change for different gradient directions
+              end: Alignment.bottomRight,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Center buttons vertically
+              children: [
+                const Text(
+                  'Hello Parent!',
+                  style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Space buttons evenly
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.white60,
+                        borderRadius: BorderRadius.circular(20), // Rounded corners
+                      ),
+                      padding: const EdgeInsets.all(0),
+                      child: IconButton(
+                        icon: const ImageIcon(
+                          AssetImage('lib/images/turn_taking.png'),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const TurnTakingPage(title: 'Turn Taking',)),
+                          );
+                        },
+                        iconSize: 60.0,
+                      ),
+                    ),
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.white60,
+                        borderRadius: BorderRadius.circular(20), // Rounded corners
+                      ),
+                      padding: const EdgeInsets.all(0),
+                      child: IconButton(
+                        icon: const ImageIcon(
+                          AssetImage('lib/images/contact.png'),
+                        ),
+                        onPressed: () {
+                          // Add functionality for button 1
+                        },
+                        iconSize: 60.0,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Turn Taking',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Contact Us',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ]
+                ),
+                const SizedBox(height: 22),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Space buttons evenly
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.white60,
+                        borderRadius: BorderRadius.circular(20), // Rounded corners
+                      ),
+                      padding: const EdgeInsets.all(0),
+                      child: IconButton(
+                        icon: const ImageIcon(
+                          AssetImage('lib/images/calendar.png'),
+                        ),
+                        onPressed: () {
+                          // Add functionality for button 1
+                        },
+                        iconSize: 60.0,
+                      ),
+                    ),
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.white60,
+                        borderRadius: BorderRadius.circular(20), // Rounded corners
+                      ),
+                      padding: const EdgeInsets.all(0),
+                      child: IconButton(
+                        icon: const ImageIcon(
+                          AssetImage('lib/images/trends.png'),
+                        ),
+                        onPressed: () {
+                          // Add functionality for button 1
+                        },
+                        iconSize: 60.0,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'My Day',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Trends',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ]
+                ),
+                const SizedBox(height: 22),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Space buttons evenly
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.white60,
+                        borderRadius: BorderRadius.circular(20), // Rounded corners
+                      ),
+                      padding: const EdgeInsets.all(0),
+                      child: IconButton(
+                        icon: const ImageIcon(
+                          AssetImage('lib/images/strategy.png'),
+                        ),
+                        onPressed: () {
+                          // Add functionality for button 1
+                        },
+                        iconSize: 60.0,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'ASE Strategies',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ]
+                ),
+              ]
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TurnTakingPage extends StatefulWidget {
+  const TurnTakingPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<TurnTakingPage> createState() => _TurnTakingPage();
+}
+
+class _TurnTakingPage extends State<TurnTakingPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 600,
+          width: 400,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+              colors: [Colors.blueAccent[400]!, Colors.lightBlue[100]!, Colors.yellow[400]!], // Adjust color shades as desired
+              begin: Alignment.topLeft, // Change for different gradient directions
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+              top: 16.0, // Optional padding
+              left: 16.0, // Optional padding
+              child: IconButton(
+                icon: const Icon(Icons.home),
+                iconSize: 60.0,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Home Page',)),
+                  );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
